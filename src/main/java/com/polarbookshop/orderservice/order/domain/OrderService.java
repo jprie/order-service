cf4378058ff052fa9a4bd63c0acd4a7a475c8b7e
Johannes Priebsch <johannes@priebsch.at>
2024-03-15 11:46:19 +0100

    add publish order accepted

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -2,18 +2,28 @@ package com.polarbookshop.orderservice.order.domain;
 
 import com.polarbookshop.orderservice.book.Book;
 import com.polarbookshop.orderservice.book.BookClient;
+import com.polarbookshop.orderservice.order.event.OrderAcceptedMessage;
 import com.polarbookshop.orderservice.order.event.OrderDispatchedMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class OrderService {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
+    private final StreamBridge streamBridge;
     private final OrderRepository orderRepository;
 
     private final BookClient bookClient;
 
-    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+    public OrderService(StreamBridge streamBridge, OrderRepository orderRepository, BookClient bookClient) {
+        this.streamBridge = streamBridge;
         this.orderRepository = orderRepository;
         this.bookClient = bookClient;
     }
@@ -22,13 +32,16 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
+
+    @Transactional // make DB and Messaging an atomic operation
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> buildAcceptedOrder(book, quantity))
                 .defaultIfEmpty(
                         buildRejectedOrder(isbn, quantity)
                 )
-                .flatMap(orderRepository::save);
+                .flatMap(orderRepository::save)
+                .doOnNext(this::publishOrderAcceptedEvent);
     }
 
 
@@ -49,6 +62,20 @@ public class OrderService {
                 .flatMap(orderRepository::save);
     }
 
+    private void publishOrderAcceptedEvent(Order order) {
+
+        if (!order.status().equals(OrderStatus.ACCEPTED)) {
+            return;
+        }
+
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event with id {}", order.id());
+        var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}",
+                order.id(), result);
+
+    }
+
     private Order buildDispatchedOrder(Order existingOrder) {
         return new Order(
                 existingOrder.id(),
@@ -62,4 +89,6 @@ public class OrderService {
                 existingOrder.version()
         );
     }
+
+
 }
